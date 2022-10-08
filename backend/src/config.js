9724ccf75aae35633004dc2a154f18c56097ffb5
Riohoo R <ronald2@foxmail.com>
2022-10-08 14:58:55 +0800

    update for rehabor 10.8

--- backend/src/config.js
+++ backend/src/config.js
@@ -50,14 +50,14 @@ const OWNER_ADDRESS = '0x5f7Bc15EA39F52c859c5Be9A1686e2180D96ba42';
 const TREASURY_ADDRESS = '0x5f7Bc15EA39F52c859c5Be9A1686e2180D96ba42';
 const MAX_SUPPLY = 3888; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.01; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-10-04T11:30:00+08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x5f7Bc15EA39F52c859c5Be9A1686e2180D96ba42"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
